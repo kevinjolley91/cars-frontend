5559e51ad38fac5a0e7a9d6d622660237399343b
kevinjolley91 <jolley427@gmail.com>
2024-01-09 15:29:11 -0500

    adds manufacturers index

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { ManufacturersIndex } from "./ManufacturersIndex";
+
 export function Content() {
+  const [manufacturers, setManufacturers] = useState([]);
+
+  const handleIndexManufacturers = () => {
+    console.log("handleIndexManufacturers");
+    axios.get("http://localhost:3000/manufacturers.json").then((response) => {
+      console.log(response.data);
+      setManufacturers(response.data);
+    });
+  };
+
+  useEffect(handleIndexManufacturers, []);
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <ManufacturersIndex manufacturers={manufacturers} />
     </div>
   );
 }
---
+++ src/ManufacturersIndex.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+export function ManufacturersIndex(props) {
+  return (
+    <div>
+      <h1>All Manufacturers</h1>
+      {props.manufacturers.map((manufacturer) => (
+        <div key={manufacturer.id}>
+          <h2>{manufacturer.name}</h2>
+          <img src={manufacturer.img_logo} height="150" width="300" alt={manufacturer.name} />
+          <p>Year Established: {manufacturer.year_created}</p>
+          <p>Country of Origin: {manufacturer.country}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
